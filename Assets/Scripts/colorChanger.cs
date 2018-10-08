8e2567efcafb122e9018589a4c339d2d57c4659e
william.arvidsson <william.arvidsson@elev.ga.lbs.se>
2018-10-08 18:32:30 +0200

    Colour Changer

---
+++ Assets/Scripts/colorChanger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChanger : MonoBehaviour
+{
+    //The ship variable that allows modification of the sprite
+    public SpriteRenderer shipRenderer;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Makes it so when you press/hold down the "A" key the ship turns green       
+        if (Input.GetKey(KeyCode.A))
+        {
+            shipRenderer.color = new Color(0f, 1f, 0f, 1f);
+        }
+        //Makes it so when you press/hold down the "D" key the ship turns blue
+        if (Input.GetKey(KeyCode.D))
+        {
+            shipRenderer.color = new Color(0f, 0f, 1f, 1f);
+        }
+        // Makes it so that when you press the "Space" key the ship changes into a random colour
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shipRenderer.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1);
+        }
+    }
+}
--- Assets/Scripts/shipMovement.cs
+++ Assets/Scripts/shipMovement.cs
@@ -11,26 +11,35 @@ public class shipMovement : MonoBehaviour
     //How much slower the ship turning left is than the ship turning right
     [Range (0.1f,1f)]
     public float leftSlowerRatio;
-    //Player assigned ship speed
+    //assigned ship speed
     public float baseSpeed;
     //Current ship speed
     float forwardSpeed;
-    //The ship variable
-    public SpriteRenderer shipRenderer;
     //a timer for time spent in game
     int timer;
+    //The current location of the ship
     public Transform shipLocation;
-    
+    //Give the player the choice if they want random speed or not
+    public bool randomShipSpeed;
+    //Gives the player the choice if they want a random start location or not
+    public bool randomStartPosition;
     // Use this for initialization
     void Start()
     {
 
-        //Sets the current ship speed to the player assigned ship speed
-        //Sets the timer to 1
         //Sets the left rotation speed to be slower which is based on the leftSlowerRatio
-        forwardSpeed = baseSpeed;
         leftRotationSpeed = baseRotationSpeed*leftSlowerRatio;
+        //Sets the timer to 1
         timer = 1;
+        //Gives the ship a random start position within view if randomStartPosition is enablaled
+        shipLocation.position = new Vector3(Random.Range(-8f, 8f), Random.Range(4.5f,-4.5f));
+        //Makes it so that if the player enabled random speed it makes the ship start speed random
+        if (randomShipSpeed == true)
+        {
+        baseSpeed = Random.Range(1, 11);
+        }
+        //Sets the current ship speed to the player assigned ship speed
+        forwardSpeed = baseSpeed;
     }
 
     // Update is called once per frame
@@ -39,17 +48,15 @@ public class shipMovement : MonoBehaviour
         //This makes the ship go forward based on given speed, in real time
         transform.Translate(forwardSpeed * Time.deltaTime, 0, 0);
 
-        //Makes it so when you press/hold down the "A" key the ship turn left, also turns the ship green       
+        //Makes it so when you press/hold down the "A" key the ship turn left,
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(0f, 0f, leftRotationSpeed * Time.deltaTime);
-            shipRenderer.color = new Color(0f,1f,0f,1f);
         }
-        //Makes it so when you press/hold down the "D" key the ship turn right, also turns the ship blue
+        //Makes it so when you press/hold down the "D" key the ship turn right, 
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0f, 0f, -baseRotationSpeed * Time.deltaTime);
-            shipRenderer.color = new Color(0f,0f,1f,1f);
         }
         //Makes it so when you press/hold down the "S" key the current ship speed halfs
         if (Input.GetKey(KeyCode.S))
@@ -71,14 +78,17 @@ public class shipMovement : MonoBehaviour
             print("Timer: " + timer);
             timer = timer + 1;
         }
-        // Makes it so that when you press the "Space" key the ship changes colour
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            shipRenderer.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1);
-        }
+
+        //makes it so when the ship comes to a position that is of screen it warps to the other side so that it goes into view again
+        //this is done for both the x position and y position
         if (transform.position.x > 10.5 || transform.position.x <-10.5)
         {
             shipLocation.position = new Vector3 (shipLocation.position.x * -1, shipLocation.position.y);
         }
+        if (transform.position.y > 6.5 || transform.position.y < -6.5)
+        {
+            shipLocation.position = new Vector3(shipLocation.position.x , shipLocation.position.y * -1);
+        }
+
     }
 }
