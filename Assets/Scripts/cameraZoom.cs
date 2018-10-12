9118a601ef4b6863688367361d16d13a0e60117a
william.arvidsson <william.arvidsson@elev.ga.lbs.se>
2018-10-12 15:50:51 +0200

    StartZoom grej

---
+++ Assets/Scripts/cameraZoom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraZoom : MonoBehaviour
+{
+    public float startCameraZoom = 5;
+    public float currentCameraZoom;
+    [Range(0.000001f,1f)]
+    public float zoomIncreaseSpeed;
+    // Use this for initialization
+    void Start()
+    {
+        currentCameraZoom = startCameraZoom;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentCameraZoom = currentCameraZoom + (startCameraZoom * zoomIncreaseSpeed) * Time.deltaTime;
+        Camera.main.orthographicSize = currentCameraZoom;
+    }
+}
--- Assets/Scripts/shipMovement.cs
+++ Assets/Scripts/shipMovement.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class shipMovement : MonoBehaviour
 {
+    //Player assigned rotation speed
+    public float startRotationSpeed;
     //The ships speed turning right.
-    public float baseRotationSpeed;
+    public float rightRotationSpeed;
     //The ships speed turning left
     float leftRotationSpeed;
     //How much slower the ship turning left is than the ship turning right
     [Range (0.1f,1f)]
     public float leftSlowerRatio;
-    //assigned ship speed
-    public float baseSpeed;
+    //player assigned ship speed
+    public float startSpeed = 5;
     //Current ship speed
-    float forwardSpeed;
+    float baseSpeed;
     //a timer for time spent in game
     int timer;
     //The current location of the ship
@@ -31,6 +33,9 @@ public class shipMovement : MonoBehaviour
     public SpriteRenderer randomShip3;
     //Variable for which ship the game will show
     int randomShip;
+    //The speed at which the ship will accelerate at
+    [Range (1f, 0.0000001f)]
+    public float speedIncrease;
     
     // Use this for initialization
     void Start()
@@ -56,8 +61,12 @@ public class shipMovement : MonoBehaviour
             randomShip3.enabled = true;
         }
 
+        //Sets the rightRotationSpeed to startRotationSpeed
+        rightRotationSpeed = startRotationSpeed;
+        //Sets the leftRotationSpeed to startRotationSpeed
+        leftRotationSpeed = startRotationSpeed;
         //Sets the left rotation speed to be slower which is based on the leftSlowerRatio
-        leftRotationSpeed = baseRotationSpeed*leftSlowerRatio;
+        leftRotationSpeed = rightRotationSpeed*leftSlowerRatio;
         //Sets the timer to 1
         timer = 1;
         //Gives the ship a random start position within view if randomStartPosition is enablaled
@@ -68,27 +77,17 @@ public class shipMovement : MonoBehaviour
         //Makes it so that if the player enabled random speed it makes the ship start speed random
         if (randomShipSpeed == true)
         {
-        baseSpeed = Random.Range(1, 11);
+        startSpeed = Random.Range(1, 11);
         }
         //Sets the current ship speed to the player assigned ship speed
-        forwardSpeed = baseSpeed;
+        baseSpeed = startSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
         //This makes the ship go forward based on given speed, in real time
-        transform.Translate(forwardSpeed * Time.deltaTime, 0, 0);
-        //makes it so when you press the "W" key the ship speed doubles
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            forwardSpeed = baseSpeed * 2;
-        }
-        //When you release the "W" key the ships speed resets to the base speed
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            forwardSpeed = baseSpeed;
-        }
+        transform.Translate(baseSpeed * Time.deltaTime, 0, 0);
         //Makes it so when you press/hold down the "A" key the ship turn left, independent of framerate
         if (Input.GetKey(KeyCode.A))
         {
@@ -97,18 +96,18 @@ public class shipMovement : MonoBehaviour
         //Makes it so when you press/hold down the "D" key the ship turn right, independent of framerate
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(0f, 0f, -baseRotationSpeed * Time.deltaTime);
+            transform.Rotate(0f, 0f, -rightRotationSpeed * Time.deltaTime);
         }
         //Makes it so when you press/hold down the "S" key the current ship speed halfs
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            forwardSpeed = baseSpeed / 2;
+            baseSpeed = baseSpeed / 2;
             
         }
         //When the "S"key is up it sets the current ship speed back to the player assigned speed
         if (Input.GetKeyUp(KeyCode.S))
         {
-            forwardSpeed = baseSpeed;
+            baseSpeed = baseSpeed*2;
         }
 
         //Prints the time spent in game each second.
@@ -130,6 +129,10 @@ public class shipMovement : MonoBehaviour
         {
             shipLocation.position = new Vector3(shipLocation.position.x , shipLocation.position.y * -1);
         }
-
+        //Makes it so that the ships speed will increase based on speedIncrease and time spent in game 
+        baseSpeed = baseSpeed+(startSpeed * speedIncrease)*Time.deltaTime;
+        //Increases both left and right rotation speed base on speedIncrease and time spent in game 
+        leftRotationSpeed = leftRotationSpeed + (startRotationSpeed * speedIncrease) * Time.deltaTime;
+        rightRotationSpeed = rightRotationSpeed + (startRotationSpeed * speedIncrease) * Time.deltaTime;
     }
 }
